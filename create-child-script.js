e8b0b4c8506a84e9406aad760c02b0fc41c50eed
AndreaSavojardoTeoresi <andrea.savojardo@teoresigroup.com>
2025-08-27 10:56:06 +0200

    Create create-child-script.js

---
+++ create-child-script.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+
+async function main() {
+  // Il payload del webhook è accessibile tramite la variabile d'ambiente.
+  const webhookPayload = JSON.parse(process.env.WEBHOOK_PAYLOAD);
+  const pat = process.env.PAT_TOKEN;
+
+  const parentWorkItemId = webhookPayload.resource.id;
+  const parentWorkItemTitle = webhookPayload.resource.fields["System.Title"];
+  const organizationUrl = webhookPayload.resource.url.split('/_apis/wit')[0];
+  
+  const newWorkItemDocument = [{
+    "op": "add",
+    "path": "/fields/System.Title",
+    "value": `Nuovo Work Item 'Prova' per: ${parentWorkItemTitle}`
+  },
+  {
+    "op": "add",
+    "path": "/fields/System.WorkItemType",
+    "value": "Prova"
+  },
+  {
+    "op": "add",
+    "path": "/relations/-",
+    "value": {
+      "rel": "System.LinkTypes.Hierarchy-Reverse",
+      "url": `${organizationUrl}/_apis/wit/workitems/${parentWorkItemId}`
+    }
+  }];
+  
+  try {
+    await axios.patch(
+      `${organizationUrl}/_apis/wit/workitems/$${newWorkItemDocument[1].value}?api-version=6.0`,
+      newWorkItemDocument, {
+        headers: {
+          'Content-Type': 'application/json-patch+json',
+          'Authorization': 'Basic ' + Buffer.from(':' + pat).toString('base64')
+        }
+      }
+    );
+    console.log('Work Item creato con successo.');
+  } catch (error) {
+    console.error('Errore nella creazione del Work Item:', error.response ? error.response.data : error.message);
+    process.exit(1); // Esci con un codice di errore
+  }
+}
+
+main();
